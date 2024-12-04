8f819724ec40b78b859ae05beb7047ad8593a8fc
DziedzicGrzegorz <grzegorzdziedzic198@gmail.com>
2024-12-04 12:50:53 +0100

    feat: add sidebar

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import {SidebarDemo} from "@/components/ui/SidebarDemo";
+
+const DashboardLayout = ({children}: { children: React.ReactNode }) => {
+    return <SidebarDemo>{children}</SidebarDemo>;
+};
+
+export default DashboardLayout;
